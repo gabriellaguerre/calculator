678f3f6625575897c0830476cdf678dadafdb121
gabriellaguerre <laguerre.g.g@gmail.com>
2022-12-03 22:12:20 -0500

    calculator design

---
+++ calculator.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+    //build calculator frame
+    const frame = document.createElement("div")
+    frame.setAttribute("class", "frame")
+    document.body.appendChild(frame)
+
+    //identify each grid
+
+
+})
