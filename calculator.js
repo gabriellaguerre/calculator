31948744a4bdd3bb39db9a599acb53b471e56256
gabriellaguerre <laguerre.g.g@gmail.com>
2022-12-04 08:43:13 -0500

    design improved

--- calculator.js
+++ calculator.js
@@ -6,12 +6,18 @@ window.addEventListener("DOMContentLoaded", () => {
 
     //creating each grid button
     for(let i = 1; i <= 16; i++) {
-        let grid = document.createElement("div")
-        grid.setAttribute("id", "grids")
-        grid.innerText = i
+        let grid = document.createElement("button")
+        grid.setAttribute("id", `grids${i}`)
+        grid.classList.add("buttons")
+        grid.innerText = i;
         frame.appendChild(grid)
     }
 
-
-
+   //creating results bar
+   const results = document.createElement("div")
+   results.setAttribute("id", "result")
+   document.body.append(results)
+   results.innerText = 0;
 })
+
+    //calculator buttons
