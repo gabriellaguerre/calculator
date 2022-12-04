083c59ee804014de434c8e55263a56f90f944326
gabriellaguerre <laguerre.g.g@gmail.com>
2022-12-04 17:47:05 -0500

    design draft completed --> started operation fxnalities

--- calculator.js
+++ calculator.js
@@ -13,71 +13,99 @@ window.addEventListener("DOMContentLoaded", () => {
         frame.appendChild(grid)
     }
 
-   //creating results bar
-   const results = document.createElement("div")
+   //results bar
+   const results = document.createElement("input")
    results.setAttribute("id", "result")
    document.body.append(results)
-   results.innerText = '';
 
 
-   //calculator Number buttons
+   //calculator number buttons
     const button1 = document.getElementById("grids1")
     button1.addEventListener("click", () => {
-        results.innerText += 1;
+        results.value += 1;
+
     })
     const button2 = document.getElementById("grids2")
     button2.addEventListener("click", () => {
-        results.innerText += 2;
+        results.value += 2;
     })
     const button3 = document.getElementById("grids3")
     button3.addEventListener("click", () => {
-        results.innerText += 3;
+        results.value += 3;
     })
     const button4 = document.getElementById("grids5")
     button4.innerText = 4;
     button4.addEventListener("click", () => {
-        results.innerText += 4;
+        results.value += 4;
     })
     const button5 = document.getElementById("grids6")
     button5.innerText = 5;
     button5.addEventListener("click", () => {
-        results.innerText += 5;
+        results.value += 5;
     })
     const button6 = document.getElementById("grids7")
     button6.innerText = 6;
     button6.addEventListener("click", () => {
-        results.innerText += 6;
+        results.value += 6;
     })
     const button7 = document.getElementById("grids9")
     button7.innerText = 7;
     button7.addEventListener("click", () => {
-        results.innerText += 7;
+        results.value += 7;
     })
     const button8 = document.getElementById("grids10")
     button8.innerText = 8;
     button8.addEventListener("click", () => {
-        results.innerText += 8;
+        results.value += 8;
     })
     const button9 = document.getElementById("grids11")
     button9.innerText = 9;
     button9.addEventListener("click", () => {
-        results.innerText += 9;
+        results.value += 9;
     })
-
-
 
 
     //calculator operation buttons
     const buttonClear = document.getElementById("grids4")
     buttonClear.innerText = "clear"
     buttonClear.addEventListener("click", () => {
-        results.innerText = ' ';
+        results.value = ' ';
     })
     const buttonPlus = document.getElementById("grids8")
     buttonPlus.innerText = "+"
     buttonPlus.addEventListener("click", () => {
-        results.innerText += " + ";
+        results.value += " + ";
+
+    })
+    const buttonMinus = document.getElementById("grids12")
+    buttonMinus.innerText = "-"
+    buttonMinus.addEventListener("click", () => {
+        results.value += " - ";
+    })
+    const buttonEquals = document.getElementById("grids16")
+    buttonEquals.innerText = "="
+    buttonEquals.addEventListener("click", () => {
+        localStorage.setItem("operation", results.value)
+
+    })
+    const buttonTimes = document.getElementById("grids15")
+    buttonTimes.innerText = "x"
+    buttonTimes.addEventListener("click", () => {
+        results.value += " x ";
+    })
+    const buttonDivide = document.getElementById("grids14")
+    buttonDivide.innerText = "/"
+    buttonDivide.addEventListener("click", () => {
+        results.value += " / ";
+    })
+    const buttonPeriod = document.getElementById("grids13")
+    buttonPeriod.innerText = "."
+    buttonPeriod.style.fontSize = "30px"
+    buttonPeriod.addEventListener("click", () => {
+        results.value += ".";
     })
 
+    //mathematical operations
+    
 
 })
